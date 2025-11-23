0609c581b6bc086322e1c5d64d2655b937cda3eb
TassyH <tassy.lazame24@gmail.com>
2025-11-22 22:42:55 -0300

    testa pipeline

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
 app.listen(3001, () => {
-  console.log("API de Login TDD rodando na porta 3001");
+  console.log("API de Login TDD rodando na porta 3001 meu parca");
 });
